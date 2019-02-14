5c5d02148029c1131f716abad97f6fc7b267fd4c
sysbaram <sysbaram@gmail.com>
2019-02-14 13:32:22 +0900

    사원증출력

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ from blog.views import index, hello_times
 from blog.views import articles_by_year
 from blog.views import naver_realtime_keywords
 from blog.views import naver_blog_search
+from blog.views import 사원증_이미지_응답
 
 from django.urls import register_converter
 from blog.converters import FourDigitYearConverter, SlugUnicodeConverter
@@ -29,5 +30,5 @@ urlpatterns = [
 
     path('naver/실시간검색어/', naver_realtime_keywords),
     path('naver/네이버블로그검색/', naver_blog_search),
-
+    path('사원증/', 사원증_이미지_응답),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests
 from bs4 import BeautifulSoup
+from io import BytesIO
+from PIL import Image, ImageDraw, ImageFont
 
 # Create your views here.
 
@@ -58,3 +60,35 @@ def naver_blog_search(request):
     else:
         text = '검색어를 지정해주세요.'
     return HttpResponse(text)
+
+
+    def 사원증_이미지_응답(request):
+        # ttf_path = 'C:/Windows/Fonts/malgun.ttf' # 윈도우, 맥: 
+        # ttf_path = '/Library/Fonts/AppleGothic.ttf'
+
+        ttf_path = 'assets/fonts/AppleGothic.ttf'
+        text = request.GET.get('name', '익명')
+
+        image_url = 'http://www.flowermeaning.com/flower-pics/Calla-Lily-Meaning.jpg'
+        res = requests.get(image_url) # 서버로 HTTP GET 요청하여, 응답 획득
+
+        io = BytesIO(res.content) # 응답의 Raw Body  메모리 파일 객체 BytesIO 인스턴스 생성
+        io.seek(0) # 파일의 처음으로 커서를 이동
+        canvas = Image.open(io).convert('RGBA') # 이미지 파일을 열고, RGBA 모드로 변환
+        font = ImageFont.truetype(ttf_path, 40) # 지정 경로의 TrueType 폰트, 폰트크기 40
+        draw = ImageDraw.Draw(canvas) # canvas에 대한 ImageDraw 객체 획득
+                                        
+        left, top = 10, 10
+        margin = 10
+        width, height = font.getsize(text)
+        right = left + width + margin
+        bottom = top + height + margin
+
+        draw.rectangle((left, top, right, bottom), (255, 255, 224))
+        draw.text((15,15), text, font=font, fill=(20, 20, 20))
+
+          # canvas.show()  # 뷰에서는 필요가 없어서, 주석처리
+
+        response = HttpResponse(content_type='image/png')
+        canvas.save(response, format='PNG')    # HttpResponse 의 file-like 특성 활용
+        return response
