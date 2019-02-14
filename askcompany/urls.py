f75c838ba45ce41781c2d91634bc789515bedee9
sysbaram <sysbaram@gmail.com>
2019-02-14 09:45:04 +0900

    work add

--- askcompany/urls.py
+++ askcompany/urls.py
@@ -22,6 +22,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),	 
     path('blog/', include('blog.urls')),
-    path('shop/', include('shop.urls')),   
+    path('shop/', include('shop.urls')),
+    path('work/', include('work.urls')),   
 #    path('', index),
 ]
