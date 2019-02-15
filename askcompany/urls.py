8dcb2c9b73f1c1725652d3b81b79b00104695ef4
sysbaram <sysbaram@gmail.com>
2019-02-15 15:23:26 +0900

    5th fllow

--- askcompany/urls.py
+++ askcompany/urls.py
@@ -17,12 +17,18 @@ Including another URLconf
 
 
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 from django.urls import include, path
+
+def root(request):
+    # TODO: 차후에 URL Reverse 기능
+    return HttpResponseRedirect('/blog/')
 
 urlpatterns = [
     path('admin/', admin.site.urls),	 
     path('blog/', include('blog.urls')),
     path('shop/', include('shop.urls')),
     path('work/', include('work.urls')),   
+    path('', root),
 #    path('', index),
 ]
